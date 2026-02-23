e83a1e39f40933ee7ee6fcbdea8cfe96568294e3
Niko Novak <novakniko2000@gmail.com>
2026-02-23 17:38:36 +0100

    Storing Data in db, getting events from db

--- main.go
+++ main.go
@@ -19,7 +19,11 @@ func main() {
 }
 
 func getEvents(context *gin.Context) {
-	events := models.GetAllEvents()
+	events, err := models.GetAllEvents()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to retrieve events."})
+		return
+	}
 	context.JSON(http.StatusOK, gin.H{
 		"events": events,
 	})
@@ -39,7 +43,12 @@ func createEvent(context *gin.Context) {
 	event.ID = 1
 	event.UserID = 1
 
-	event.Save()
+	err = event.Save()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to create events."})
+		return
+	}
 
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 }
